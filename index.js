9c2d8008622ba2f4e01252dca8e8480c9b69385c
Tamás Németh <tomuwhu@gmail.com>
2018-11-23 14:28:41 +0100

    .

--- index.js
+++ index.js
@@ -6,27 +6,19 @@ app.use(bodyParser.json())
 var sse = new Map
 var allmsg = []
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
+var os = require('os')
+var ifaces = os.networkInterfaces()
 Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
-
+    var alias = 0
     ifaces[ifname].forEach(function (iface) {
-        if ('IPv4' !== iface.family || iface.internal !== false) {
-            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-            return;
-        }
-
+        if ('IPv4' !== iface.family || iface.internal !== false) return
         if (alias >= 1) {
-            // this single interface has multiple ipv4 addresses
-            console.log(ifname + ':' + alias, iface.address);
+            console.log(ifname + ':' + alias, iface.address)
         } else {
-            // this interface has only one ipv4 adress
-            console.log(ifname, iface.address);
+            console.log(ifname, iface.address)
         }
-        ++alias;
-    });
+        ++alias
+    })
 })
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html') )
